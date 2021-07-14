6c0e598ec56d88bc6b3669fece05e38db5107412
Jori <jvalkenhoff@outlook.com>
2021-07-14 23:38:07 +0200

    Added spotify api call, search handler

--- client/src/Dashboard.tsx
+++ client/src/Dashboard.tsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useReducer, useEffect } from 'react';
 import useAuth from './useAuth';
+import InputBase from '@material-ui/core/InputBase';
+import Search from './Search';
 
 interface Props {
-    code: String,
+    code: string,
+}
+
+type State = { 
+    input: string
+}
+
+const initialState:State = {
+    input: ''
+}
+
+type Action = { type: 'setSearch', payload: string }
+
+const reducer = (state: State, action: Action): State => {
+    return {
+        ...state,
+        input: action.payload
+    }
 }
 
 export const Dashboard = (prop: Props) => {
     const accessToken = useAuth(prop.code);
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        document.title = state.input
+    }, [state.input])
+
+    const handleSearchChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        dispatch({
+            type: 'setSearch',
+            payload: event.target.value
+        })
+    }
+
     return (
         <div>
+            <Search />
             {prop.code}
         </div>
     )
--- client/src/useAuth.tsx
+++ client/src/useAuth.tsx
@@ -2,25 +2,50 @@ import {useState, useEffect} from 'react'
 import axios from 'axios';
 import Tokens from './auth/Tokens';
 
+// https://open.spotify.com/artist/1Xyo4u8uXC1ZmMpatF05PJ?si=3kGy8SN9R0eWQQpBX5iRhw&dl_branch=1
 
-const useAuth = (code: String) => {
-    const tokens = Tokens.getInstance();
+const useAuth = (code: string) => {
     const [accessToken, setAccessToken] = useState();
     const [refreshToken, setResfreshToken] = useState();
     const [expiresIn, setExpiresIn] = useState();
+    const [tokenType, setTokenType] = useState();
 
-    // useEffect(() => {
-    //     axios.post('http://localhost:3001/login', {
-    //         code,
-    //     }).then(res => {
-    //         const verifier = generateRandomString();
-    //         console.log(verifier);
-    //         console.log(res.data)
-    //         window.history.pushState({}, '', '/')
-    //     }).catch(() => {
-    //         window.location.href = '/';
-    //     })
-    // }, [code])
+    useEffect(() => {
+        const params = new URLSearchParams();
+        const tokens = Tokens.getInstance();
+        const verifier = tokens.getVerifier()!;
+
+        params.append('client_id', '72bd15495cfe4f5f8418be092849b08a');
+        params.append('grant_type', 'authorization_code');
+        params.append('code', code);
+        params.append('redirect_uri', 'http://localhost:3000')
+        params.append('code_verifier', verifier);
+
+        axios.post('https://accounts.spotify.com/api/token', params, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+        .then(res => {
+            // setAccessToken(res.data.access_token);
+            // setResfreshToken(res.data.refresh_token);
+            // setExpiresIn(res.data.expires_in);
+            // setTokenType(res.data.token_type);
+
+            axios.get('https://api.spotify.com/v1/artists/1Xyo4u8uXC1ZmMpatF05PJ', {
+                headers: {
+                    'Authorization': res.data.token_type + ' ' + res.data.access_token,
+                }
+            }).then(res => {
+                console.log(res.data);
+            }).catch(err => {
+                console.log(err);
+            })
+            // window.history.pushState({}, '', '/')
+        }).catch(err => {
+            console.log(err);
+        })
+    })
 }
 
 export default useAuth;
